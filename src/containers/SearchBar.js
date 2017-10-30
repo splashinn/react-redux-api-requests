6e4527b339961451c3fcdb69928c27c9e59a6e49
Literall <pnguyen@icgconsulting.com>
2017-10-30 00:15:31 -0700

    add redux middleware and more

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -1,10 +1,35 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+   constructor() {
+       super();
+
+       this.state = {
+           term: ''
+       }
+
+       this.inputChange = this.inputChange.bind(this);
+       this.submitTerm = this.submitTerm.bind(this);
+   }
+
+   inputChange(event) {
+       console.log(event.target.value);
+       this.setState({
+        term: event.target.value
+       });
+   }
+
+   submitTerm(event) {
+       event.preventDefault();
+       console.log('-* state value *-', this.state.term)
+       document.querySelector('input').value = '';
+       document.querySelector('input').focus();
+   }
+
     render() {
         return (
-            <form className="input-group">
-            <input type="text" placeholder="Enter a city name" required="true" />
+            <form onSubmit={this.submitTerm} className="input-group">
+            <input onChange={this.inputChange} value={this.state.term} className="form-control" type="text" placeholder="Enter a city name" required="true" />
             <span className="input-group=btn">
                 <button className="btn-btn-secondary" type="submit">Submit</button>
             </span>
